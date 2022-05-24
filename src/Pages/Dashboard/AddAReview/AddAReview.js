e38380f2efcd9f793a31f7ee9241bb149a7bd468
sabbirzzaman <sztonmoy7@gmail.com>
2022-05-24 14:53:04 +0600

    review page added

--- src/Pages/Dashboard/AddAReview/AddAReview.js
+++ src/Pages/Dashboard/AddAReview/AddAReview.js
@@ -1,10 +1,61 @@
+import axios from 'axios';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useQuery } from 'react-query';
+import auth from '../../../firebase.init';
+import Loader from '../../Common/Loader/Loader';
+import ReviewTable from '../ReviewTable/ReviewTable';
 import './AddAReview.css'
 
 const AddAReview = () => {
+    // get user information
+    const [user] = useAuthState(auth);
+
+    // get user orders data
+    const { data, isLoading } = useQuery('orders', () =>
+        axios.get(`http://localhost:5000/orders?email=${user?.email}`)
+    );
+
+    if(isLoading) {
+        return <Loader height="50vh"></Loader>
+    }
+
+    const orders = data?.data;
+
+    const paidOrders = orders.filter(order => order.paid === true)
+
     return (
-        <div>
-            <h3>Add A review</h3>
+        <div className="order-container">
+            {orders.length ? (
+                <>
+                    <h3>Add Reviews</h3>
+                    <table className='review-table'>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Product Name</th>
+                                <th>Price</th>
+                                <th>Transaction Id</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {paidOrders.map((order, index) => (
+                                <ReviewTable
+                                    key={order._id}
+                                    order={order}
+                                    index={index}
+                                ></ReviewTable>
+                            ))}
+                        </tbody>
+                    </table>
+                </>
+            ) : (
+                <>
+                    <h3>You don't placed any orders yet!</h3>
+                    <button className="btn">Order Now</button>
+                </>
+            )}
         </div>
     );
 };
---
+++ src/Pages/Dashboard/ReviewTable/ReviewTable.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './ReviewTable.css'
+
+const ReviewTable = ({order, index}) => {
+    const { _id, productName, price, transactionId, paid } = order;
+
+    const navigate = useNavigate();
+    return (
+        <tr>
+            <td>{index + 1}</td>
+            <td>{productName}</td>
+            <td>${price}.00</td>
+            <td>
+                {transactionId}
+            </td>
+            <td>
+                {!paid ? (
+                    <>
+                        <button
+                            style={{
+                                backgroundColor: '#2ecc71',
+                                width: '100%',
+                            }}
+                            className="pay"
+                            onClick={() => navigate(`/payment/${_id}`)}
+                        >
+                            Pay
+                        </button>
+                        <button
+                            style={{
+                                backgroundColor: '#e74c3c',
+                                width: '100%',
+                            }}
+                            className="cancel"
+                        >
+                            Cancel
+                        </button>
+                    </>
+                ) : (
+                    <>
+                        <button
+                            style={{ backgroundColor: '#2ecc71' }}
+                            className="status"
+                        >
+                            Details
+                        </button>
+                    </>
+                )}
+            </td>
+        </tr>
+    );
+};
+
+export default ReviewTable;
